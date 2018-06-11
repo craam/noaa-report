1a95d9c8c7a93e6371ce72cc7abf8ea6394937ce
Edison Neto <ednetoali@gmail.com>
2018-06-11 18:09:36 -0300

    new file:   MANIFEST.in 	new file:   noaareport/__init__.py 	renamed:    noaareport.py -> noaareport/noaareport.py 	new file:   setup.py
    Created the package structure.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="noaareport",
+    version="0.1",
+    author="Edison Neto",
+    author_email="ednetoali@gmail.com",
+    packages=['noaareport'],
+    include_package_data=True,
+    install_requires=[
+        'pandas'
+    ],
+)
+
