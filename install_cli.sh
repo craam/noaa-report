9fb735f04e2987ae99d02b1a3272406bf6dc63c3
Edison Neto <ednetoali@gmail.com>
2018-10-19 16:38:00 -0300

    Finish install cli script

--- install_cli.sh
+++ install_cli.sh
@@ -5,7 +5,6 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
-$path=$1
-wget "https://gist.githubusercontent.com/3ldr0n/93786e07549f1ef546275fae0d9290f9/raw/bc4a51f05547c482ddc09c637f153ad4e0a8b0a7/report.py"
-cp report.py /usr/bin/noaareport
+wget "https://gist.githubusercontent.com/3ldr0n/93786e07549f1ef546275fae0d9290f9/raw/544f435809f9552eabe7d6f75e1235e91d77da3f/report.py"
+sed 's#path = 0#path = "'$1'"#g' report.py > /usr/bin/noaareport
 chmod +x /usr/bin/noaareport
