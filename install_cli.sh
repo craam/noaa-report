950c2a3da80e6a102e1247537090f9af5da0fbf1
Edison Neto <ednetoali@gmail.com>
2018-12-15 10:24:02 -0200

    Update install cli script to install into /usr/local

--- install_cli.sh
+++ install_cli.sh
@@ -7,4 +7,4 @@ fi
 
 wget "https://gist.githubusercontent.com/3ldr0n/93786e07549f1ef546275fae0d9290f9/raw/544f435809f9552eabe7d6f75e1235e91d77da3f/report.py"
 sed 's#path = 0#path = "'$1'"#g' report.py > /usr/bin/noaareport
-chmod +x /usr/bin/noaareport
+chmod +x /usr/local/bin/noaareport
